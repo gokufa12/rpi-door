e0ea9e08affeccd6618af5eb543a51d4af27f6b2
Brady Hutchins <bhutchins@factset.com>
2017-05-11 11:50:05 -0400

    websockets work

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var socketIO = require('socket.io');
 var path = require('path');
+
+var PORT = process.env.PORT || 8000;
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
-
-var port = process.env.PORT || 8000;
-
-app.listen(port, function() {
-    console.log('Example app listening on port ' + port);
+var server = app.listen(PORT, function() {
+    console.log('Listening on port ' + PORT);
 });
+
+var io = socketIO(server);
+io.on('connection', function(socket) {
+    console.log('client connected');
+    socket.on('disconnect', function() {
+        console.log('client disconnected');
+    });
+});
+
+setInterval( function() {
+    io.emit('time', new Date().toTimeString())
+}, 1000);
