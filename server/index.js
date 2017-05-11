56245450fa4226a1ce414ad060d47820e6335b18
Brady Hutchins <bhutchins@factset.com>
2017-05-11 11:18:02 -0400

    respect port srsly

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 var port = process.env.PORT || 8000;
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('Example app listening on port ' + port);
 });
