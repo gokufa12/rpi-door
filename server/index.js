ea2edadb9c13c1da96e79e55649dd48d73f56030
Brady Hutchins <bhutchins@factset.com>
2017-05-11 13:41:27 -0400

    Added fake initial data and fake update data

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var express = require('express');
 var socketIO = require('socket.io');
 var path = require('path');
@@ -12,13 +14,50 @@ var server = app.listen(PORT, function() {
 });
 
 var io = socketIO(server);
-io.on('connection', function(socket) {
+
+var clientChannel = io.of('/client');
+
+var pis = {
+    one: {
+        id: 'one',
+        room: 'flex room 1',
+        status: 'available'
+    },
+    two: {
+        id: 'two',
+        room: 'flex room 2',
+        status: 'taken'
+    }
+};
+var clientSockets = [];
+
+clientChannel.on('connection', function(socket) {
     console.log('client connected');
+
+    clientSockets.push(socket);
+    socket.emit('initial-state', pis);
+
     socket.on('disconnect', function() {
         console.log('client disconnected');
+        _.pull(clientSockets, socket);
     });
 });
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+_.forEach(pis, function(pi) {
+   var setStatus = function() {
+        pi.status = pi.status === 'taken' ? 'available' : 'taken';
+        clientChannel.emit('room-update', pi);
+        setTimeout(setStatus, getRandomInt(2500, 10000))
+    };
+    setTimeout(setStatus, getRandomInt(2500, 10000))
+});
+
 setInterval( function() {
-    io.emit('time', new Date().toTimeString())
+    clientChannel.emit('time', new Date().toTimeString())
 }, 1000);
